4abbcf632547da7471d534c30cc894e86b23bcec
Oblivion <fengredrum@163.com>
2023-06-28 23:02:19 +0800

    Fix a tiny bug

--- data_utils.py
+++ data_utils.py
@@ -74,7 +74,7 @@ class TextAudioLoader(torch.utils.data.Dataset):
             # raise ValueError("{} {} SR doesn't match target {} SR".format(
             #     sampling_rate, self.sampling_rate))
         # audio_norm = audio / self.max_wav_value
-        audio_norm = audio_norm.unsqueeze(0)
+        # audio_norm = audio_norm.unsqueeze(0)
         spec_filename = filename.replace(".wav", ".spec.pt")
         if os.path.exists(spec_filename):
             spec = torch.load(spec_filename)

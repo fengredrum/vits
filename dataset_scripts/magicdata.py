6730ca83ebecf9fe7bcf23f057fd413180c964fb
Oblivion <fengredrum@163.com>
2023-07-04 23:04:59 +0800

    Merge two datasets from Magic Data

--- dataset_scripts/magicdata.py
+++ dataset_scripts/magicdata.py
@@ -1,59 +1,58 @@
 import os
-import re
 import argparse
 import random
+import json
 
 from tqdm import tqdm
+
+random.seed(1234)
 
 speakers = {"G0051": "0", "G0052": "1", "G0053": "2",
             "G0054": "3", "G0055": "4", "G0067": "5",
             "G0068": "6", "G0070": "7", "G0071": "8",
-            "G0072": "9"}
+            "G0072": "9", "G0001": "10", "G0002": "11",
+            "G0003": "12", "G0004": "13", "G0005": "14",
+            "G0006": "15", "G0007": "16", "G0008": "17",
+            "G0009": "18", "G0010": "19",
+            }
 
+with open('data/magicdata/speakers_map.json', 'w') as outfile:
+    json.dump(speakers, outfile)
 
-def get_audiopath_textnorm(filepath):
-    with open(filepath, "r") as f:
-        filetext = f.read()
-    lines = filetext.split("\n")
-    lines = lines[1:]
-    random.seed(1234)
-    random.shuffle(lines)
+def get_audiopath_textnorm(filepaths):
+    all_lines = []
+    for filepath in filepaths:
+        print(f"Processing on {filepath}.")
+        if os.path.exists(filepath):
+            with open(filepath, "r") as f:
+                filetext = f.read()
+            lines = filetext.split("\n")
+            lines = lines[1:]
+            random.shuffle(lines)
+            all_lines.extend(lines)
+        else:
+            print(f"Could not find {filepath}!")
 
-    for l in lines:
+    for l in all_lines:
         if len(l) > 0:
             try:
-                _, uttrans_id, speaker_id, prompt, transcription = l.split(
-                    "\t")
-                text = "[GD]" + transcription + "[GD]"
-                audio_path = "WAV/" + speaker_id + "/" + uttrans_id
+                _, uid, sid, prompt, trans = l.split("\t")
+                text = "[GD]" + trans + "[GD]"
+                audio_path = "WAV/" + sid + "/" + uid
                 audiopath_and_text = "|".join(
-                    [audio_path, speakers[speaker_id], text]) + "\n"
+                    [audio_path, speakers[sid], text]) + "\n"
                 yield audiopath_and_text
             except:
                 print(f'Skipping line: {l}, length: {len(l)}.')
 
 
-def main(dataset_path, output_dir):
-    # Obtain the dataset directories and collect tsv files
-    print(f"Processing on {dataset_path}.")
-
-    if os.path.exists(dataset_path):
-        with open(output_dir, "w", encoding="utf-8") as f:
-            f.writelines(get_audiopath_textnorm(dataset_path))
-    else:
-        print(f"Could not find {dataset_path}!")
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--outdir", default="./filelists/magicdata_audio_sid_text_")
+        "--outdir", default="./filelists/magicdata_audio_sid_text_train_filelist.txt")
     parser.add_argument("--filelists", nargs="+",
-                        default=["./data/magicdata/UTTRANSINFO.txt", ])
-
+                        default=["./data/magicdata/UTTRANSINFO.txt", "./data/magicdata/UTTERANCEINFO.txt"])
     args = parser.parse_args()
 
-    for filepath in args.filelists:
-        new_filelist = args.outdir
-        new_filelist += "train_filelist.txt"
-        main(filepath, new_filelist)
+    with open(args.outdir, "w", encoding="utf-8") as f:
+        f.writelines(get_audiopath_textnorm(args.filelists))

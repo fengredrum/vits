689b604cb93bad6e7b3dc9977c29a1e13007671e
Oblivion <fengredrum@163.com>
2023-07-05 09:38:02 +0800

    Dump speaker id

--- dataset_scripts/aishell3.py
+++ dataset_scripts/aishell3.py
@@ -1,30 +1,35 @@
 import os
-import re
 import argparse
+import json
+import random
 
 from tqdm import tqdm
+
+random.seed(1234)
+speakers = {}
 
 
 def get_audiopath_textnorm(filepath, filename):
     with open(filepath + "/" + filename, "r") as f:
         filetext = f.read()
     lines = filetext.split("\n")
+    lines = lines[5:]
+    random.shuffle(lines)
 
     sid = -1
-    readers = {}
-    for l in lines[5:]:
+    for l in lines:
         if len(l) > 1:
             try:
                 audio_path, pinyin, text = l.split("|")
                 text = "[ZH]" + text + "[ZH]"
-                reader_id = audio_path[:7]
-                if reader_id not in readers:
+                speaker_id = audio_path[:7]
+                if speaker_id not in speakers:
                     sid += 1
-                    readers.update({reader_id: str(sid)})
+                    speakers.update({speaker_id: str(sid)})
 
-                audio_path = filepath + "/wav/" + reader_id + "/" + audio_path + ".wav"
+                audio_path = filepath + "/wav/" + speaker_id + "/" + audio_path + ".wav"
                 audiopath_and_text = "|".join(
-                    [audio_path, readers[reader_id], text]) + "\n"
+                    [audio_path, speakers[speaker_id], text]) + "\n"
                 yield audiopath_and_text
             except:
                 print(f'Skipping line: {l}, length: {len(l)}.')
@@ -59,3 +64,6 @@ if __name__ == "__main__":
         new_filelist = args.outdir
         new_filelist += filepath.split("/")[-1] + "_filelist.txt"
         main(filepath, new_filelist)
+
+    with open('data/AISHELL-3/speakers_map.json', 'w') as outfile:
+        json.dump(speakers, outfile)
